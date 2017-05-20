0002886c4ea3f3340cd7ee76904d6fc4047c4227
Unknown <pisdak79@gmail.com>
2017-05-21 02:11:47 +0530

    Index page
    Bhai lag rha hai ki ho gaya hai ab nhi pata. Bolo mata rani ki jay!!!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,50 @@
-<<<<<<< HEAD
 <html>
-    <head>
-        <!--Import Google Icon Font-->
-        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!--Import materialize.css-->
-        <link href="{{ asset("/css/materialize.min.css") }}" rel="stylesheet" media="screen,projection" type="text/css">
-        <link href="{{ asset("/css/animate.css") }}" rel="stylesheet" type="text/css">
-        <link type="text/css" rel="stylesheet" href="css/animate.css">
-        <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-
-      
-    </head>
-
+    @include('layouts.header')
     <body style="font-family: 'Raleway', sans-serif;">
-        <!--Import jQuery before materialize.js-->
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript" src="{{ asset("/js/materialize.min.js") }}"></script>
-        <script type="text/javascript">
-            $(document).ready(function(){
-                $('.tooltipped').tooltip({delay: 50});
-            });
-        </script>
-      
-        @include('layouts.header')
-        
-        <br><br>
+    <!--Import jQuery before materialize.js-->
+        <div class="navbar-fixed">
+            <nav class="transparent black-text">
+                <div class="nav-wrapper" style="z-index: 1;" >
+                    <a href="#" class="brand-logo" style="padding-left: 60px; color: #ef5350;">
+                        <img style="width: 130px; height: 45px; padding-top: 10px;" src="{{asset('/img/logo.svg') }}">
+                    </a>
+
+                    <ul id="nav-mobile" class="right hide-on-med-and-down black-text">
+                        <li ><a href="" class="black-text">Self Care</a></li>
+                        <li ><a href="" class="black-text">Solutions</a></li>
+                        <li ><a href="" class="black-text">Recharge</a></li>
+                        <li ><a href="" class="black-text">Feedback</a></li>
+                        @if(Auth::guest())
+                            <li ><a href="{{ route('login') }}" class="black-text">Login</a></li>
+                            <li ><a href="{{ route('register') }}" class="black-text">Register</a></li>
+                        @else
+                            <ul id="dropdown1" class="dropdown-content">
+                                <li>
+                                    <a href="" class="black-text">{{ Auth::user()->name }}</a>
+                                </li>
+                                <li>
+                                    <a class="black-text" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="post" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                            <li>
+                                <a class="dropdown-button" href="#" data-activates="dropdown1">
+                                    <div class="black-text">{{ Auth::user()->name }}</div>
+                                    <i class="material-icons right">arrow_drop_down</i>
+                                </a>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    <br><br>
         
         @section('container')
         @show
@@ -35,87 +52,3 @@
         @include('layouts.footer')
     </body>
 </html>
-=======
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <script src="{{ asset('js/jquery.js') }}"></script>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        @yield('content')
-    </div>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
->>>>>>> master
